1b73bdf7f4ff4a70a5da7b2a7891f7a94663f4bb
Wojciech <51675015+studentwb@users.noreply.github.com>
2020-06-13 17:26:25 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,7 +13,7 @@
   * the "License"; You may not use this file except in compliance with the
   * License. You may obtain a copy of the License at:
   *                        opensource.org/licenses/BSD-3-Clause
-  *
+  *Projekt realiozwany w ramach studiów inżynierskich automatyka i robotyka 
   ******************************************************************************
   */
 /* USER CODE END Header */
